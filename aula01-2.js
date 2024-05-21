8533aa7464fb25950174653298b59dee5a431d4a
WESLEY ALESSANDRO DARÉ ANTUNES <wesley_a_antunes@estudante.sesisenai.org.br>
2024-05-21 20:54:44 -0300

    Meu primeiro commit

---
+++ aula01-2.js
@@ -0,0 +1,16 @@
+var qg = prompt("Digite a quantidade de gols")
+var qc = prompt("Digite a quantidade de passes certos")
+var qe = prompt("Digite a quantidade de passes errados")
+var pontos = (qg * 50) + (qc * 10) + (qe * -5)
+
+if (pontos < 50) {
+    document.write("Péssima partida");
+} else if (pontos >= 50 && pontos < 100) {
+    document.write("Partida Ruim");
+} else if (pontos >= 100 && pontos < 150) {
+    document.write("Fez o Básico");
+} else if (pontos >= 150 && pontos < 200) {
+    document.write("Jogou Bem");
+} else {
+    document.write("Jogou demais");
+}
