518532d3bd212c53457591276991aa1e49a7bd3e
AynHol <wesley_a_antunes@estudante.sesisenai.org.br>
2024-05-22 19:49:42 -0300

    teste 3

--- aula02.js
+++
@@ -1,9 +0,0 @@
-var nome = "Wesley Alessandro Dare Antunes"
-var idade = 19
-var eProfessor = false
-var dataNascimento
-
-document.write(typeof nome, '<br/>')
-document.write(typeof dataNascimento, '<br/>')
-document.write(typeof eProfessor, '<br/>')
-document.write(typeof idade)
